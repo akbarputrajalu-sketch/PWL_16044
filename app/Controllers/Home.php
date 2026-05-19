78765e24468b42b843f054bc09ed3f16acb180d5
akbar putra <akbarputraalu@gmail.com>
2026-05-19 17:31:02 +0700

    Read data and validation

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,21 @@
 
 namespace App\Controllers;
 
+use App\Models\ProductModel;
+
 class Home extends BaseController
 {
-    public function index(): string
+    protected $productModel;
+
+    function __construct()
     {
-        return view('v_home');
+        $this->productModel = new ProductModel();
+    }
+    public function index()
+    {
+        $products = $this->productModel->findAll();
+        $data['products'] = $products;
+
+        return view('v_home', $data);
     }
 }
